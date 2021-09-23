e48cfc10ef02d9b2e810835a441fc01c878dc121
JoaoZati <joaozati@gmail.com>
2021-09-23 20:25:55 -0300

    sqlite_script1.py - Criando funções para manipular o sql lite

--- Database selection/sqlite_script1.py
+++ Database selection/sqlite_script1.py
@@ -26,4 +26,23 @@ def view():
     return rows
 
 
+def delete_item(item):
+    conection = sqlite3.connect("lite.db")
+    cursor = conection.cursor()
+    cursor.execute(f"DELETE FROM store WHERE item='{item}'")
+    conection.commit()
+    conection.close()
+
+
+def update_item(item, quantity, price):
+    conection = sqlite3.connect("lite.db")
+    cursor = conection.cursor()
+    cursor.execute(f"UPDATE store SET quantity={quantity}, price={price} WHERE item='{item}'")
+    conection.commit()
+    conection.close()
+
+
+#insert('Wine Glass', 10, 5.5)
+#update_item('Wine Glass', 8, 5.8)
+# delete_item("Wine Glass")
 print(view())
